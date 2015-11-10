027577eeb9122e1a60d9dc6f7f2ad90ab4963fff
Tobias Becker <tobias_b@rbg.informatik.tu-darmstadt.de>
2015-11-10 11:50:02 +0100

    added small swing gui

--- Dream2/src/test/java/javareact/chat/Chat.java
+++ Dream2/src/test/java/javareact/chat/Chat.java
@@ -1,36 +1,102 @@
 package javareact.chat;
 
+import java.awt.Container;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.SequentialGroup;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 import javareact.common.Consts;
 import javareact.common.types.ReactiveChangeListener;
 import javareact.common.types.RemoteVar;
 import javareact.common.types.Var;
 
-public class Chat implements ReactiveChangeListener<String> {
+public class Chat extends JFrame implements ReactiveChangeListener<String> {
 
 	private RemoteVar<String> remoteMessages;
 	private Var<String> messages;
 	private String userName;
-
+	private JTextArea msgs;
+	private JTextField sendText;
 	public Chat(String username) throws Exception {
 		Consts.hostName = username;
 		messages = new Var<String>("message", "");
 		remoteMessages = new RemoteVar<String>("message@*");
 		remoteMessages.addReactiveChangeListener(this);
 		this.userName = username;
-	}
+        initUI();
+    }
 
-	protected void writeMessage(String text) {
-		messages.set(userName + ":" + text);
+    private void initUI() {
+    	sendText = new JTextField(20);
+    	sendText.addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent e) {
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER)
+					 sendMessage();
+			}
+		});
+    	JButton sendButton = new JButton("Send");
+    	sendButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                sendMessage();
+            }
+        });
+    	msgs = new JTextArea(5, 20);
+    	msgs.setEditable(false);
+    	JPanel panel = new JPanel();
+    	panel.add(msgs);
+	    panel.add(sendText);
+	    panel.add(sendButton);
+	    getContentPane().add(panel);  
+	    
+        setTitle("Chat - " + userName);
+        setSize(300, 200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+
+	protected void sendMessage() {
+		messages.set(userName + ":" + sendText.getText());
+		displayMessage("You: " + sendText.getText());
+		sendText.setText("");
+	}
+	
+	protected void displayMessage(String text) {
+		if (msgs.getText().isEmpty())
+			msgs.append(text);
+		else
+			msgs.append(System.lineSeparator() + text);
 	}
 
 	@Override
 	public void notifyReactiveChanged(String newValue) {
 		String[] msg = newValue.split(":", 2);
 		if (!msg[0].equals(userName))
-			System.out.println(newValue);
+			displayMessage(msg[0] + ": " +msg[1]);
 	}
 
 	public static void main(String[] args) {
@@ -38,21 +104,30 @@ public class Chat implements ReactiveChangeListener<String> {
 			if (args.length < 1)
 				System.out.println("username missing");
 
-			// args[0]=topicName; args[1]=username
-			Chat chat = new Chat(args[0]);
-
-			// read from command line
-			BufferedReader commandLine = new java.io.BufferedReader(
-					new InputStreamReader(System.in));
-
-			// loop until the word "exit" is typed
-			while (true) {
-				String s = commandLine.readLine();
-				if (s.equalsIgnoreCase("exit")) {
-					System.exit(0);// exit program
-				} else
-					chat.writeMessage(s);
-			}
+	        EventQueue.invokeLater(new Runnable() {
+	            
+	            @Override
+	            public void run() {
+					try {
+						Chat chat = new Chat(args[0]);
+						chat.setVisible(true);
+					} catch (Exception e) {
+					}
+	            	
+	            }
+	        });
+//			// read from command line
+//			BufferedReader commandLine = new java.io.BufferedReader(
+//					new InputStreamReader(System.in));
+//
+//			// loop until the word "exit" is typed
+//			while (true) {
+//				String s = commandLine.readLine();
+//				if (s.equalsIgnoreCase("exit")) {
+//					System.exit(0);// exit program
+//				} else
+//					chat.writeMessage(s);
+//			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}

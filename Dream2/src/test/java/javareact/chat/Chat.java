4e7c2fd06f8ae58aeeb012fff8038a4a655ccea7
Tobias Becker <tobias_b@rbg.informatik.tu-darmstadt.de>
2015-11-12 13:07:13 +0100

    added online status as signal

--- Dream2/src/test/java/javareact/chat/Chat.java
+++ Dream2/src/test/java/javareact/chat/Chat.java
@@ -19,6 +19,7 @@ import javax.swing.SpringLayout;
 import javareact.common.Consts;
 import javareact.common.types.ReactiveChangeListener;
 import javareact.common.types.RemoteVar;
+import javareact.common.types.Signal;
 import javareact.common.types.Var;
 
 public class Chat extends JFrame implements ReactiveChangeListener<String> {
@@ -26,140 +27,157 @@ public class Chat extends JFrame implements ReactiveChangeListener<String> {
 	private static final long serialVersionUID = 390641070042167681L;
 	private RemoteVar<String> remoteMessages;
 	private Var<String> messages;
+	private Var<Boolean> status;
 	private String userName;
 	private JTextArea msgs;
 	private JTextField sendText;
 	private JList<String> statusList;
-	
+
 	public Chat(String username) throws Exception {
 		Consts.hostName = username;
 		messages = new Var<String>("message", "");
 		remoteMessages = new RemoteVar<String>("message@*");
 		remoteMessages.addReactiveChangeListener(this);
-		
-		this.userName = username;
-        initUI();
-    }
 
-    private void initUI() {
-    	sendText = new JTextField(20);
-    	sendText.addKeyListener(new KeyListener() {
-			
+		status = new Var<Boolean>("status", true);
+		RemoteVar<Boolean> remoteStatus = new RemoteVar<>("status@*");
+		Signal<Boolean> s = new Signal<Boolean>("s", () -> {
+			if (remoteStatus.get() == null)
+				return false;
+			else
+				return remoteStatus.get();
+		} , remoteStatus);
+		s.addReactiveChangeListener(new ReactiveChangeListener<Boolean>() {
+
+			@Override
+			public void notifyReactiveChanged(Boolean newValue) {
+				System.out.println(newValue);
+
+			}
+		});
+		this.userName = username;
+		initUI();
+	}
+
+	private void initUI() {
+		sendText = new JTextField(20);
+		sendText.addKeyListener(new KeyListener() {
+
 			@Override
 			public void keyTyped(KeyEvent e) {
 			}
-			
+
 			@Override
 			public void keyReleased(KeyEvent e) {
 			}
-			
+
 			@Override
 			public void keyPressed(KeyEvent e) {
 				if (e.getKeyCode() == KeyEvent.VK_ENTER)
-					 sendMessage();
+					sendMessage();
 			}
 		});
-    	JButton sendButton = new JButton("Send");
-    	sendButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent event) {
-                sendMessage();
-            }
-        });
-    	msgs = new JTextArea(5, 27);
-    	msgs.setEditable(false);
-    	msgs.setMaximumSize(null);
-    	
-    	DefaultListModel<String> listModel = new DefaultListModel<String>();
-    	listModel.addElement("Alice");
-    	listModel.addElement("Bob");
-    	statusList = new JList<String>(listModel);
-    	statusList.setEnabled(false);
-    	statusList.setLayoutOrientation(JList.HORIZONTAL_WRAP);
-    	statusList.setVisibleRowCount(-1);
-    	
-    	statusList.setCellRenderer(new DefaultListCellRenderer() {
-    		
+		JButton sendButton = new JButton("Send");
+		sendButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				sendMessage();
+			}
+		});
+		msgs = new JTextArea(5, 27);
+		msgs.setEditable(false);
+		msgs.setMaximumSize(null);
+
+		DefaultListModel<String> listModel = new DefaultListModel<String>();
+		listModel.addElement("Alice");
+		listModel.addElement("Bob");
+		statusList = new JList<String>(listModel);
+		statusList.setEnabled(false);
+		statusList.setLayoutOrientation(JList.HORIZONTAL_WRAP);
+		statusList.setVisibleRowCount(-1);
+
+		statusList.setCellRenderer(new DefaultListCellRenderer() {
+
 			private static final long serialVersionUID = 9019815674349211344L;
 			private JLabel label = new JLabel();
-		    private Color textSelectionColor = Color.BLACK;
-		    private Color backgroundSelectionColor = Color.CYAN;
-		    private Color textNonSelectionColor = Color.BLACK;
-		    private Color backgroundNonSelectionColor = Color.WHITE;
+			private Color textSelectionColor = Color.BLACK;
+			private Color backgroundSelectionColor = Color.CYAN;
+			private Color textNonSelectionColor = Color.BLACK;
+			private Color backgroundNonSelectionColor = Color.WHITE;
 
-    		@Override
-    		public java.awt.Component getListCellRendererComponent(javax.swing.JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
-    			
-    			String name = (String)value;
-                label.setIcon(createImageIcon("status-offline.png", "Offline"));
-                label.setText(name);
+			@Override
+			public java.awt.Component getListCellRendererComponent(javax.swing.JList<?> list, Object value, int index,
+					boolean isSelected, boolean cellHasFocus) {
 
-                if (isSelected) {
-                    label.setBackground(backgroundSelectionColor);
-                    label.setForeground(textSelectionColor);
-                } else {
-                    label.setBackground(backgroundNonSelectionColor);
-                    label.setForeground(textNonSelectionColor);
-                }
+				String name = (String) value;
+				label.setIcon(createImageIcon("status-offline.png", "Offline"));
+				label.setText(name);
 
-                return label;
-    		};
+				if (isSelected) {
+					label.setBackground(backgroundSelectionColor);
+					label.setForeground(textSelectionColor);
+				} else {
+					label.setBackground(backgroundNonSelectionColor);
+					label.setForeground(textNonSelectionColor);
+				}
 
-    	});
-	    SpringLayout layout = new SpringLayout();
-	    
-	    // put messages on (5,5)
-	    layout.putConstraint(SpringLayout.WEST, msgs, 5, SpringLayout.WEST, getContentPane());
-	    layout.putConstraint(SpringLayout.NORTH, msgs, 5, SpringLayout.NORTH, getContentPane());
-	    
-	    // put textfield below messages
-	    layout.putConstraint(SpringLayout.NORTH, sendText, 5, SpringLayout.SOUTH, msgs);
-	    layout.putConstraint(SpringLayout.WEST, sendText, 5, SpringLayout.WEST, getContentPane());
-	    
-	    // put button next to the textfield
-	    layout.putConstraint(SpringLayout.NORTH, sendButton, 5, SpringLayout.SOUTH, msgs);
-	    layout.putConstraint(SpringLayout.WEST, sendButton, 5, SpringLayout.EAST, sendText);
-	    
-	    // make the frame big enough to fit all in
-	    layout.putConstraint(SpringLayout.EAST, getContentPane(), 10, SpringLayout.EAST, statusList);
-	    layout.putConstraint(SpringLayout.SOUTH, getContentPane(), 10, SpringLayout.SOUTH, sendText);
-	    
-	    layout.putConstraint(SpringLayout.NORTH, statusList, 5, SpringLayout.NORTH, getContentPane());
-	    layout.putConstraint(SpringLayout.WEST, statusList, 15, SpringLayout.EAST, sendButton);
-	    
-	    getContentPane().setLayout(layout);
-	    
-	    getContentPane().add(msgs);  
-	    getContentPane().add(sendText);  
-	    getContentPane().add(sendButton);  
-	    getContentPane().add(statusList);
-	    
-        setTitle("Chat - " + userName);
-//        setSize(300, 200);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        
-        pack();
-    }
+				return label;
+			};
 
-    /** Returns an ImageIcon, or null if the path was invalid. */
-    protected ImageIcon createImageIcon(String path,
-                                               String description) {
-        java.net.URL imgURL = getClass().getResource(path);
-        if (imgURL != null) {
-            return new ImageIcon(imgURL, description);
-        } else {
-            System.err.println("Couldn't find file: " + path);
-            return null;
-        }
-    }
+		});
+		SpringLayout layout = new SpringLayout();
+
+		// put messages on (5,5)
+		layout.putConstraint(SpringLayout.WEST, msgs, 5, SpringLayout.WEST, getContentPane());
+		layout.putConstraint(SpringLayout.NORTH, msgs, 5, SpringLayout.NORTH, getContentPane());
+
+		// put textfield below messages
+		layout.putConstraint(SpringLayout.NORTH, sendText, 5, SpringLayout.SOUTH, msgs);
+		layout.putConstraint(SpringLayout.WEST, sendText, 5, SpringLayout.WEST, getContentPane());
+
+		// put button next to the textfield
+		layout.putConstraint(SpringLayout.NORTH, sendButton, 5, SpringLayout.SOUTH, msgs);
+		layout.putConstraint(SpringLayout.WEST, sendButton, 5, SpringLayout.EAST, sendText);
+
+		// make the frame big enough to fit all in
+		layout.putConstraint(SpringLayout.EAST, getContentPane(), 10, SpringLayout.EAST, statusList);
+		layout.putConstraint(SpringLayout.SOUTH, getContentPane(), 10, SpringLayout.SOUTH, sendText);
+
+		layout.putConstraint(SpringLayout.NORTH, statusList, 5, SpringLayout.NORTH, getContentPane());
+		layout.putConstraint(SpringLayout.WEST, statusList, 15, SpringLayout.EAST, sendButton);
+
+		getContentPane().setLayout(layout);
+
+		getContentPane().add(msgs);
+		getContentPane().add(sendText);
+		getContentPane().add(sendButton);
+		getContentPane().add(statusList);
+
+		setTitle("Chat - " + userName);
+		// setSize(300, 200);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		pack();
+	}
+
+	/** Returns an ImageIcon, or null if the path was invalid. */
+	protected ImageIcon createImageIcon(String path, String description) {
+		java.net.URL imgURL = getClass().getResource(path);
+		if (imgURL != null) {
+			return new ImageIcon(imgURL, description);
+		} else {
+			System.err.println("Couldn't find file: " + path);
+			return null;
+		}
+	}
 
 	protected void sendMessage() {
 		messages.set(userName + ":" + sendText.getText());
 		displayMessage("You: " + sendText.getText());
 		sendText.setText("");
 	}
-	
+
 	protected void displayMessage(String text) {
 		if (msgs.getText().isEmpty())
 			msgs.append(text);
@@ -171,7 +189,7 @@ public class Chat extends JFrame implements ReactiveChangeListener<String> {
 	public void notifyReactiveChanged(String newValue) {
 		String[] msg = newValue.split(":", 2);
 		if (!msg[0].equals(userName))
-			displayMessage(msg[0] + ": " +msg[1]);
+			displayMessage(msg[0] + ": " + msg[1]);
 	}
 
 	public static void main(String[] args) {
@@ -179,30 +197,30 @@ public class Chat extends JFrame implements ReactiveChangeListener<String> {
 			if (args.length < 1)
 				System.out.println("username missing");
 
-	        EventQueue.invokeLater(new Runnable() {
-	            
-	            @Override
-	            public void run() {
+			EventQueue.invokeLater(new Runnable() {
+
+				@Override
+				public void run() {
 					try {
 						Chat chat = new Chat(args[0]);
 						chat.setVisible(true);
 					} catch (Exception e) {
 					}
-	            	
-	            }
-	        });
-//			// read from command line
-//			BufferedReader commandLine = new java.io.BufferedReader(
-//					new InputStreamReader(System.in));
-//
-//			// loop until the word "exit" is typed
-//			while (true) {
-//				String s = commandLine.readLine();
-//				if (s.equalsIgnoreCase("exit")) {
-//					System.exit(0);// exit program
-//				} else
-//					chat.writeMessage(s);
-//			}
+
+				}
+			});
+			// // read from command line
+			// BufferedReader commandLine = new java.io.BufferedReader(
+			// new InputStreamReader(System.in));
+			//
+			// // loop until the word "exit" is typed
+			// while (true) {
+			// String s = commandLine.readLine();
+			// if (s.equalsIgnoreCase("exit")) {
+			// System.exit(0);// exit program
+			// } else
+			// chat.writeMessage(s);
+			// }
 		} catch (Exception e) {
 			e.printStackTrace();
 		}

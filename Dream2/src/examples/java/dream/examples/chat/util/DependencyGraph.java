65a433718175c5a10c8456d4822ddf8d44ef17b4
Tobias Becker <tobias_b@rbg.informatik.tu-darmstadt.de>
2016-05-04 22:03:59 +0200

    added clustering of host to visualization of dependency graph

--- Dream2/src/examples/java/dream/examples/chat/util/DependencyGraph.java
+++ Dream2/src/examples/java/dream/examples/chat/util/DependencyGraph.java
@@ -4,8 +4,11 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 
 public class DependencyGraph {
 
@@ -28,8 +31,32 @@ public class DependencyGraph {
 		return toDot(nodes, "forward", "1.0");
 	}
 
+	private static Map<String, Set<String>> addHostVar(Map<String, Set<String>> map, String hv) {
+		String[] temp = hv.split("@", 2);
+		String var = temp[0];
+		String host = temp[1];
+		if (!map.containsKey(host))
+			map.put(host, new HashSet<String>());
+		map.get(host).add(var);
+		return map;
+	}
+
 	private static String toDot(Map<String, Collection<String>> nodes, String dir, String ranksep) {
 		String s = "digraph G {\n\tdir=" + dir + ";\n\tranksep=" + ranksep + ";\n";
+		Map<String, Set<String>> hostVars = new HashMap<>();
+		for (String str : nodes.keySet()) {
+			addHostVar(hostVars, str);
+			for (String str2 : nodes.get(str)) {
+				addHostVar(hostVars, str2);
+			}
+		}
+		for (String host : hostVars.keySet()) {
+			s += "subgraph cluster_" + host + " {\n\tnode [style=filled];\n";
+			for (String var : hostVars.get(host)) {
+				s += "\t\"" + var + "@" + host + "\";\n";
+			}
+			s += "\tlabel = \"" + host + "\";\n\tcolor=blue\n}\n\n";
+		}
 		for (Entry<String, Collection<String>> e : nodes.entrySet()) {
 			for (String e2 : e.getValue()) {
 				s += "\t\"" + e.getKey() + "\" -> \"" + e2 + "\" [dir=" + dir + "];\n";

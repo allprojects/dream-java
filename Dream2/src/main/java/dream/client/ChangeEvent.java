369813ce3dcc238aacaf4260c4862ccc4b101570
Tobias Becker <tobias_b@rbg.informatik.tu-darmstadt.de>
2016-05-10 11:21:15 +0200

    ChangeEvent relies now on UUID instead of value

--- Dream2/src/main/java/dream/client/ChangeEvent.java
+++ Dream2/src/main/java/dream/client/ChangeEvent.java
@@ -2,12 +2,14 @@ package dream.client;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.UUID;
 
 import dream.common.packets.EventPacket;
 
 public class ChangeEvent<T> implements UpdateConsumer {
 
 	private T latest;
+	private UUID latestId;
 
 	private Set<ChangeEventHandler<T>> handlers = new HashSet<ChangeEventHandler<T>>();
 
@@ -30,8 +32,9 @@ public class ChangeEvent<T> implements UpdateConsumer {
 	@Override
 	public void updateFromProducer(EventPacket packet, UpdateProducer<?> producer) {
 		T newVal = (T) packet.getEvent().getVal();
-		if (latest == null || !latest.equals(newVal)) {
+		if (latest == null || !packet.getId().equals(latestId)) {
 			T old = latest;
+			latestId = packet.getId();
 			this.latest = newVal;
 			notifyHandler(old);
 		}

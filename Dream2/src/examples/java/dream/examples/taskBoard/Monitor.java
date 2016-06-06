d1b9f878836e4b4527c02196363eb194a051207c
Tobias Becker <tobias_b@rbg.informatik.tu-darmstadt.de>
2016-06-06 15:58:39 +0200

    taskBoard: re-added button and changed display functionality

--- Dream2/src/examples/java/dream/examples/taskBoard/Monitor.java
+++ Dream2/src/examples/java/dream/examples/taskBoard/Monitor.java
@@ -1,10 +1,12 @@
 package dream.examples.taskBoard;
 
 import java.awt.Container;
+import java.awt.event.ActionEvent;
 import java.util.Arrays;
 import java.util.List;
 
 import javax.swing.GroupLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
@@ -37,7 +39,7 @@ public class Monitor extends Client {
 
 	public Monitor() {
 		super(NAME);
-		gui = new MonitorGUI();
+		gui = new MonitorGUI(this);
 
 		devs = new RemoteVar<String>(Server.NAME, Server.VAR_developers);
 		sigDevs = new Signal<String>("sigDevs", () -> {
@@ -51,17 +53,24 @@ public class Monitor extends Client {
 
 		sigDevs.change().addHandler((oldVa, newVal) -> {
 			System.out.println("newVal devs:" + newVal);
-			gui.setDevs(newVal);
+			// gui.setDevs(newVal);
 		});
 
 		sigTasks.change().addHandler((oldVa, newVal) -> {
 			System.out.println("newVal tasks:" + newVal);
-			gui.setTasks(newVal);
+			// gui.setTasks(newVal);
 		});
 	}
 
 	public static void main(String[] args) {
 		new Monitor();
+	}
+
+	public void clickButton1() {
+		readLock(toVar(Server.NAME, Server.VAR_tasks), toVar(Server.NAME, Server.VAR_developers));
+		gui.setTasks(tasks.get());
+		gui.setDevs(devs.get());
+		unlock();
 	}
 }
 
@@ -71,11 +80,15 @@ class MonitorGUI {
 	private JLabel label1;
 	private JLabel label2;
 	private JTextArea textAreaDevs;
+	private JButton button1;
+	private Monitor monitor;
 
-	public MonitorGUI() {
+	public MonitorGUI(Monitor m) {
+		this.monitor = m;
 		frame1 = new JFrame();
 		label1 = new JLabel();
 		label2 = new JLabel();
+		button1 = new JButton();
 		textAreaTasks = new JTextArea();
 		textAreaDevs = new JTextArea();
 		textAreaDevs.setEditable(false);
@@ -90,6 +103,10 @@ class MonitorGUI {
 			// ---- label2 ----
 			label2.setText("Current tasks:");
 
+			// ---- button1 ----
+			button1.setText("Show me tasks");
+			button1.addActionListener(e -> button1ActionPerformed(e));
+
 			GroupLayout frame1ContentPaneLayout = new GroupLayout(frame1ContentPane);
 			frame1ContentPane.setLayout(frame1ContentPaneLayout);
 			frame1ContentPaneLayout.setHorizontalGroup(frame1ContentPaneLayout.createParallelGroup()
@@ -100,10 +117,10 @@ class MonitorGUI {
 							.addGroup(frame1ContentPaneLayout.createParallelGroup().addComponent(label2).addComponent(
 									textAreaTasks, GroupLayout.PREFERRED_SIZE, 57, GroupLayout.PREFERRED_SIZE))
 							.addContainerGap(9, Short.MAX_VALUE))
-					.addGroup(GroupLayout.Alignment.TRAILING,
-							frame1ContentPaneLayout.createSequentialGroup().addContainerGap(11, Short.MAX_VALUE)));
+					.addGroup(GroupLayout.Alignment.TRAILING, frame1ContentPaneLayout.createSequentialGroup()
+							.addContainerGap(11, Short.MAX_VALUE).addComponent(button1).addGap(71, 71, 71)));
 			frame1ContentPaneLayout.setVerticalGroup(frame1ContentPaneLayout.createParallelGroup()
-					.addGroup(frame1ContentPaneLayout.createSequentialGroup()
+					.addGroup(frame1ContentPaneLayout.createSequentialGroup().addComponent(button1)
 							.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
 							.addGroup(frame1ContentPaneLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
 									.addComponent(label2).addComponent(label1))
@@ -117,6 +134,10 @@ class MonitorGUI {
 			frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			frame1.setVisible(true);
 		}
+	}
+
+	private void button1ActionPerformed(ActionEvent e) {
+		monitor.clickButton1();
 	}
 
 	public void setDevs(String value) {

e0b7ea6eccf88f93cf2ba7817d22f0b4cfcd7088
Tobias Becker <tobias_b@rbg.informatik.tu-darmstadt.de>
2016-06-20 14:22:47 +0200

    scrumBoard: refactored developer/task-lists to signals

--- Dream2/src/examples/java/dream/examples/scrumBoard/Server.java
+++ Dream2/src/examples/java/dream/examples/scrumBoard/Server.java
@@ -1,12 +1,10 @@
 package dream.examples.scrumBoard;
 
-import java.util.ArrayList;
 import java.util.Set;
 
 import dream.client.DreamClient;
 import dream.client.RemoteVar;
 import dream.client.Signal;
-import dream.client.Var;
 import dream.examples.util.Client;
 import dream.examples.util.Pair;
 
@@ -23,9 +21,8 @@ public class Server extends Client {
 	public static final String VAR_developers = "developers";
 	public static final String VAR_tasks = "tasks";
 
-	private final ArrayList<String> creators;
-	private final Var<String> developers;
-	private final Var<String> tasks;
+	private Pair<String, String> creator1;
+	private Pair<String, String> creator2;
 
 	public static void main(String... args) {
 		new Server();
@@ -33,47 +30,52 @@ public class Server extends Client {
 
 	public Server() {
 		super(NAME);
-		developers = new Var<String>(VAR_developers, "");
-		tasks = new Var<String>(VAR_tasks, "");
-		creators = new ArrayList<String>();
 		detectClients();
 	}
 
 	private void detectClients() {
 		Set<String> vars = DreamClient.instance.listVariables();
 		vars.stream().map(x -> new Pair<String, String>(x.split("@")[1], x.split("@")[0]))
-				.filter(x -> !creators.contains(toVar(x)) && x.getSecond().equalsIgnoreCase(Creator.VAR_newAssignment))
-				.forEach(x -> createClient(x.getFirst(), x.getSecond()));
+				.filter(x -> !creator1.equals(toVar(x)) && !creator2.equals(toVar(x))
+						&& x.getSecond().equalsIgnoreCase(Creator.VAR_newAssignment))
+				.forEach(x -> foundCreator(x));
 		try {
 			Thread.sleep(500);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		detectClients();
+
+		// stop looking after two creators have been found
+		if (creator1 == null || creator2 == null)
+			detectClients();
 	}
 
-	private void createClient(String clientHost, String clientVar) {
-		logger.info("found creator instance " + clientHost + " " + clientVar);
-		creators.add(clientVar + "@" + clientHost);
+	private void foundCreator(Pair<String, String> creator) {
+		logger.info("found creator instance " + creator);
+		if (creator1 == null)
+			creator1 = creator;
+		else if (creator2 == null)
+			creator2 = creator;
 
-		RemoteVar<Assignment> rv = new RemoteVar<>(clientHost, clientVar);
-		Signal<Assignment> sig = new Signal<>(clientHost + "-" + clientVar, () -> {
-			if (rv.get() != null) {
-				return rv.get();
-			} else {
-				return null;
-			}
-		}, rv);
-
-		sig.change().addHandler((oldValue, newValue) -> {
-			if (newValue != null) {
-				// Set vars for remote querying
-				developers.set(developers.get().length() == 0 ? newValue.getDevString()
-						: developers.get() + ":" + newValue.getDevString());
-				tasks.set(tasks.get().length() == 0 ? newValue.getTaskString()
-						: tasks.get() + ":" + newValue.getTaskString());
-				System.out.println("new value from " + clientHost + "@" + clientVar);
-			}
-		});
+		if (creator1 != null || creator2 != null)
+			initDependencies();
 	}
+
+	private void initDependencies() {
+
+		RemoteVar<Assignment> rv1 = new RemoteVar<>(creator1.getFirst(), creator1.getSecond());
+		RemoteVar<Assignment> rv2 = new RemoteVar<>(creator2.getFirst(), creator2.getSecond());
+
+		Signal<String> developers = new Signal<String>(VAR_developers, () -> {
+			// TODO
+			return "";
+		}, rv1, rv2);
+
+		Signal<String> tasks = new Signal<String>(VAR_tasks, () -> {
+			// TODO
+			return "";
+		}, rv1, rv2);
+
+	}
+
 }

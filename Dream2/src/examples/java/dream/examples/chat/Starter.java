09e482abbae01ddc27828ce3624c2470d3f4075c
Tobias Becker <tobias_b@rbg.informatik.tu-darmstadt.de>
2016-06-23 15:33:14 +0200

    refactored starter classes to ensure proper shutdown

--- Dream2/src/examples/java/dream/examples/chat/Starter.java
+++ Dream2/src/examples/java/dream/examples/chat/Starter.java
@@ -1,10 +1,8 @@
 package dream.examples.chat;
 
-import java.util.ArrayList;
-
 import dream.examples.chat.core.Chat;
 import dream.examples.chat.core.ChatServer;
-import dream.examples.util.NewJvmHelper;
+import dream.examples.util.MultipleStarter;
 
 /**
  * Convenience class to start ChatServer and x Chats (x = CHAT_COUNT), each in
@@ -16,35 +14,27 @@ public class Starter {
 
 	public static final int CHAT_COUNT = 4;
 
-	private static ArrayList<Process> processes = new ArrayList<Process>();
-
 	public static void main(String[] args) {
 		new Starter().start();
 	}
-
-	private Thread serverThread;
 
 	private static final String[] names = { "Alice", "Bob", "Chris", "David", "Eve", "Fred", "Georg", "Hans", "Igor" };
 	int xStep = 450;
 	int yStep = 175;
 
 	private void start() {
-		Runtime.getRuntime().addShutdownHook(new Thread(() -> onExit()));
-		serverThread = new Thread(() -> ChatServer.main(null));
-		serverThread.start();
-		sleep(1500);
+		MultipleStarter.addStartQueue(ChatServer.class);
 		int x = 0;
 		int y = 0;
 		for (int i = 0; i < CHAT_COUNT; i++) {
-			processes.add(NewJvmHelper.startNewJVM(Chat.class, getName(i), Integer.toString(x), Integer.toString(y)));
+			MultipleStarter.addStartQueue(Chat.class, getName(i), Integer.toString(x), Integer.toString(y));
 			x += xStep;
 			if (x >= 3 * xStep) {
 				x = 0;
 				y += yStep;
 			}
 		}
-		// sleep infinite time
-		sleep(-1);
+		MultipleStarter.start();
 	}
 
 	private String getName(int i) {
@@ -52,40 +42,5 @@ public class Starter {
 			return names[i];
 		else
 			return names[i % names.length] + "" + i;
-	}
-
-	@Override
-	protected void finalize() throws Throwable {
-		onExit();
-		super.finalize();
-	}
-
-	private void sleep(int time) {
-		do {
-			try {
-				Thread.sleep(time == -1 ? 1000 : time);
-				checkExit();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		} while (time == -1);
-
-	}
-
-	private void checkExit() {
-		for (Process p : processes) {
-			if (!p.isAlive()) {
-				System.out.println("One window closed ... exiting!");
-				System.exit(0);
-			}
-		}
-	}
-
-	private void onExit() {
-		System.out.println("exit");
-		for (Process p : processes) {
-			p.destroyForcibly();
-			System.out.println("Destroying " + p.toString());
-		}
 	}
 }
